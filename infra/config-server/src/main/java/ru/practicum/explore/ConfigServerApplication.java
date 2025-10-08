acabb4b840a57c36b020e1d4bdfb099aa4db20ee
Aleksei <andreev@almaz.spb.ru>
2025-10-08 14:40:30 +0300

    Создана модульная структура проекта с группирующими модулями infra/ и core/ Настроена микросервисная инфраструктура на базе Spring Cloud

--- infra/config-server/src/main/java/ru/practicum/explore/ConfigServerApplication.java
+++ infra/config-server/src/main/java/ru/practicum/explore/ConfigServerApplication.java
@@ -2,12 +2,14 @@ package ru.practicum.explore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
-public class StatsApplication {
+public class ConfigServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(StatsApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
